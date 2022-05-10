6dff30605cc36d1f92d7820467f3508ba12d278e
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-05-10 15:33:49 +0530

    refactors ⚙️

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { Fragment } from "react";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import About from "./components/About/About"
-import Carousel from "./components/Carousel/Carousel";
-import Footer from "./components/Footer/Footer"
+import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <Fragment>
-     <Header />
-     <Hero />
-     <About />
-     <Carousel />
-     <Footer />
+      <Header />
+      <Hero />
+      <About />
+      {/* <Carousel /> */}
+      <Footer />
     </Fragment>
   );
 }
