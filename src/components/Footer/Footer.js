2a9ca73ba32eaa6d5f0fbfddaa5f0305949d129d
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-05-10 15:59:39 +0530

    icons chnage 🤘🏻

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,21 @@
 import classes from "./Footer.module.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Grey2 from "./Grey-2.png";
-import Group from "./Group 10.png";
 import social1 from "./social1.png";
 import social2 from "./social2.png";
 import social3 from "./social3.png";
+import footer_kmct from "./footer_kmct.svg";
+import footer_karma from "./footer_karma.svg";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.elements}>
         <div className={classes.logoBox}>
-          <img className={classes.logo1} src={Grey2} alt="/" />
-          <img className={classes.logo2} src={Group} alt="/" />
+          <img src={footer_karma} alt="/" className={classes.footer_karma} />
+          <img src={footer_kmct} alt="/" className={classes.footer_kmct} />
+          {/* <img className={classes.logo1} src={Grey2} alt="/" />
+          <img className={classes.logo2} src={Group} alt="/" /> */}
         </div>
         <div className={classes.linkBox}>
           <ul className={classes.links}>
@@ -22,6 +25,7 @@ const Footer = () => {
             <li>Contact Us</li>
           </ul>
           <div className={classes.socialIcons}>
+            {/* <FontAwesomeIcon icon="fa-brands fa-instagram" /> */}
             <img className={classes.social} src={social1} alt="" />
             <img className={classes.social} src={social2} alt="" />
             <img className={classes.social} src={social3} alt="" />
