2d479f8541edb9a68d799db224d5dfa6510c3a0b
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-05-12 15:41:43 +0530

    refactors ⚙️

--- src/Routes.js
+++ src/Routes.js
@@ -2,23 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import React, { Fragment } from "react";
 import App from "./App";
-// import Culturals from "./components/Culturals/Culturals";
-// import Desafio from "./components/Culturals/Desafio/Desafio";
-// import VogueFiesta from "./components/Culturals/Vogue Fiesta/VogueFiesta";
-// import Aghaidh from "./components/Culturals/Aghaidh/Aghaidh";
+import Culturals from "./components/Culturals/Culturals";
+import Desafio from "./components/Culturals/Desafio/Desafio";
+import VogueFiesta from "./components/Culturals/Vogue Fiesta/VogueFiesta";
+import Aghaidh from "./components/Culturals/Aghaidh/Aghaidh";
 
 const MyRoutes = () => {
   return (
     <Fragment>
       <Router>
-         <Routes>
-           <Route path="/" exact element={<App />} />
-           {/* <Route path="/culturals" exact element={<Culturals />} />
-           <Route path="/desafio" exact element={<Desafio />} />
-           <Route path="/voguefiesta" exact element={<VogueFiesta />} />
-           <Route path="/aghaidh" exact element={<Aghaidh />} /> */}
-         </Routes>
-       </Router>
+        <Routes>
+          <Route path="/" exact element={<App />} />
+          <Route path="/culturals" exact element={<Culturals />} />
+          <Route path="/desafio" exact element={<Desafio />} />
+          <Route path="/voguefiesta" exact element={<VogueFiesta />} />
+          <Route path="/aghaidh" exact element={<Aghaidh />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 };
