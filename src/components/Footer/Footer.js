36c17303f598958bf4d398e75c62d1b452d7c253
Hibathulla <hibathullacm@gmail.com>
2022-05-14 17:45:30 +0530

    Footer UI Updated😘

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,101 +9,43 @@ import {
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div className={classes.elements}>
-        <div className={classes.logoBox}>
-          <img
-            src="assets/Footer/footer_karma.svg"
-            alt="/"
-            className={classes.footer_karma}
-          />
-          <img
-            src="assets/Footer/footer_kmct.svg"
-            alt="/"
-            className={classes.footer_kmct}
-          />
+        <div className={classes.row}>
+            <div className={classes.col}>
+              <img className={classes.karma} src="assets/Footer/footer_karma.svg" alt="Karma logo" />
+              <p className={classes.para}>Annual national level techno managerial cultural extravaganza of the KMCT College of Engineering. Karma enables brilliant minds to compete, discuss ideas, educate others about new technical breakthroughs, and be a part of the tech platform.</p>
+            </div>
+
+            <div className={classes.col}>
+              <h3>Contact us</h3>
+              <p>KMCT College Of Engineering, Kallanthode</p>
+              <p>NITC - P.O</p>
+              <p>Kozhikode - 673601</p>
+              <p className={classes.email}>karmakmct22@gmail.com</p>
+              <p>0495 2289227</p>
+            </div>
+
+            <div className={`${classes.col} ${classes.links}`}>
+                <h3>Links</h3>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Events</li>
+                  <li>Register</li>
+                  <li>Find us</li>
+                </ul>
+            </div>
+
+            <div className={classes.col}>
+            <h3>Follow us on</h3>
+              <div className={classes.socialIcons}>
+                <a href="https://instagram.com/karma.kmct"><FontAwesomeIcon className={classes.icons} icon={faInstagram} /></a>
+                <a href="https://www.youtube.com/channel/UCKM314A0KndJAEpvEnaxO2g"><FontAwesomeIcon className={classes.icons} icon={faYoutube} /></a>
+                <a href="https://www.facebook.com/karma.kmct/"><FontAwesomeIcon className={classes.icons} icon={faFacebook} /></a>
+              </div>
+            </div>
         </div>
-        <div className={classes.socialIcons}>
-          <a
-            className={classes.anchor}
-            href="https://www.facebook.com/karma.kmct/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className={classes.social} icon={faFacebook} />
-          </a>
-          <a
-            className={classes.anchor}
-            href="https://www.youtube.com/channel/UCKM314A0KndJAEpvEnaxO2g"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className={classes.social} icon={faYoutube} />
-          </a>
-          <a
-            className={classes.anchor}
-            href="https://instagram.com/karma.kmct"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon className={classes.social} icon={faInstagram} />
-          </a>
-        </div>
-        <div className={classes.linkBox}>
-          <ul className={classes.links}>
-            <a
-              href="https://docs.google.com/document/d/1pp2-0qGvWY1rKCDeXt6nEyd7e2XwUqDufYCE8AYVIaM/edit?usp=sharing"
-              style={{ color: "white", textDecoration: "none" }}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <li>Contact Us</li>
-            </a>
-            <li>
-              <a
-                href="https://docs.google.com/document/d/1cJfjOhoEfW_JhiMF-tUJXNWwRKxSIN-KrIlJCnh0EZ4/edit?usp=sharing"
-                style={{ color: "white", textDecoration: "none" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://docs.google.com/document/d/1VOnAodNTeF8ShOCxxVb_AXnxLKg2jKZCUY0QxbQd4nE/edit?usp=sharing"
-                style={{ color: "white", textDecoration: "none" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Terms & Conditions
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://docs.google.com/document/d/1K0En27BTf6KJB2eHZmnr7BAMDWK8T83--8HJOu-GkA0/edit?usp=sharing"
-                style={{ color: "white", textDecoration: "none" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Cancellation / Refund Policy
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://docs.google.com/document/d/1DVGB-PePPd7XCNW4LAJ6qBShudKzBJtuaAaiC6Q1DWY/edit?usp=sharing"
-                style={{ color: "white", textDecoration: "none" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Shipping Policy
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={classes.copyright}>
-        <h2>Karma'22 | All Rights Reserved © Kedilo</h2>
-      </div>
+        <hr className={classes.hr} />
+        <p className={classes.copyright}>Karma KMCT &#169; - All Rights Reserved</p>
     </footer>
   );
 };
