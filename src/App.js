7b167ce06986d1b8f73e6e9090dd87ead3fcd3b7
Hibathulla <hibathullacm@gmail.com>
2022-05-19 00:48:58 +0530

    Optimized🎄

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Fragment } from "react";
-import Hero from "./components/Hero/Hero";
-import About from "./components/About/About";
-import Events from "./components/Events/Events";
-import Ambassadar from "./components/Ambassadar/Ambassadar";
-import Alumni from "./components/Alumni/Alumni";
-import ContactUs from "./components/ContactUs/ContactUs";
-import PreLoader from "./pages/PreLoader/PreLoader";
-import Gallery from "./components/Gallery/Gallery";
+import { React ,Fragment } from "react";
+
+import Ambassadar from "./components/Ambassadar/Ambassadar"
+import Alumni from "./components/Alumni/Alumni"
+import ContactUs from "./components/ContactUs/ContactUs"
+
+const Hero = React.lazy(() => import("./components/Hero/Hero"));
+const About = React.lazy(() => import("./components/About/About"));
+const Events = React.lazy(() => import("./components/Events/Events"));
+const PreLoader = React.lazy(() => import("./pages/PreLoader/PreLoader"));
+const Gallery = React.lazy(() => import("./components/Gallery/Gallery"));
+
 
 function App() {
   return (
