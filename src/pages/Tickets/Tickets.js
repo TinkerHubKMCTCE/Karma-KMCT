621569def3bb55ed0cf071e04798c0fd013698dc
Hibathulla <hibathullacm@gmail.com>
2022-05-19 19:41:00 +0530

    Tickets section Updated🎇

--- src/pages/Tickets/Tickets.js
+++ src/pages/Tickets/Tickets.js
@@ -20,12 +20,12 @@ const Tickets = () => {
     return (
         <section className={classes.section}>
             <Background className={classes.bg}>
-            <div className={classes.headingBox}>
-                <h2 className={classes.heading}>Grab your Tickets now</h2>
-            </div>
+                <div className={classes.headingBox}>
+                    <h2 className={classes.heading}>Grab your Tickets now</h2>
+                </div>
                 <div className={classes.cards}>
 
-                    <div className={classes.card} style={{background: `url(${Bg2}), linear-gradient(${color2Prim}, ${color2Sec})`}}>
+                    <div className={classes.card} style={{ background: `url(${Bg2}), linear-gradient(${color2Prim}, ${color2Sec})` }}>
                         <div className={classes.cardSection1}>
                             <p className={classes.h1}>Day 1</p>
                             <h3 className={classes.h2}>Musical Night</h3>
@@ -36,6 +36,7 @@ const Tickets = () => {
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Entry to Inter-College Cultural Events</p>
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Hands-on Workshops and Mentoring</p>
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Entry to Expo Corner</p>
+                            <p className={classes.conditions}>* Conditions apply </p>
                         </div>
                         <div className={classes.price}>
                             <button className={classes.btn}>₹300</button>
@@ -43,7 +44,7 @@ const Tickets = () => {
                     </div>
 
 
-                    <div className={classes.card} style={{background: `url(${Bg1}), linear-gradient(${color1Prim}, ${color1Sec})`}}>
+                    <div className={classes.card} style={{ background: `url(${Bg1}), linear-gradient(${color1Prim}, ${color1Sec})` }}>
                         <div className={classes.cardSection1}>
                             <p className={classes.h1}>Day 2</p>
                             <h3 className={classes.h2}>Pro-show</h3>
@@ -54,25 +55,27 @@ const Tickets = () => {
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Featuring - <b>DJ SAMURAIZZ</b></p>
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Entry to Inclusive Hackathons</p>
                             <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Entry to Expo Corner</p>
+                            <p className={classes.conditions}>* Conditions apply </p>
                         </div>
                         <div className={classes.price}>
                             <button className={classes.btn}>₹400</button>
                         </div>
                     </div>
 
-                    
 
-                    <div className={classes.card} style={{background: `url(${Bg3}), linear-gradient(${color3Prim}, ${color3Sec})`}}>
+
+                    <div className={classes.card} style={{ background: `url(${Bg3}), linear-gradient(${color3Prim}, ${color3Sec})` }}>
                         <div className={classes.cardSection1}>
                             <p className={classes.h1}>Day 1 & 2</p>
                             <h3 className={classes.h2}>Combo</h3>
                         </div>
                         <div className={classes.details}>
-                        <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Access to <b>Musical Night</b> & <b>Pro-show</b></p>
-                        <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Inter College Cultural Events</p>
-                        <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Hackathons</p>
-                        <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Expo Corner</p>
-                        <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Exclusive mentor Talkshows</p>
+                            <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Access to <b>Musical Night</b> & <b>Pro-show</b></p>
+                            <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Inter College Cultural Events</p>
+                            <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Hackathons</p>
+                            <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Expo Corner</p>
+                            <p className={classes.detailList}><FontAwesomeIcon icon={faCheck} /> Exclusive mentor Talkshows</p>
+                            <p className={classes.conditions}>* Conditions apply </p>
                         </div>
                         <div className={classes.price}>
                             <button className={classes.btn}>₹600</button>
