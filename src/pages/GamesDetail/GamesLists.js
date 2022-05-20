54c47941bc63544fbe0cfce6b5159f40f77f1c47
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-05-20 09:51:40 +0530

    PUBG RazorPay 🔥

--- src/pages/GamesDetail/GamesLists.js
+++ src/pages/GamesDetail/GamesLists.js
@@ -6,15 +6,16 @@ const GamesLists = [
     heading: "PUBG",
     para: "KARMA'22 Is here with the PUBG- Chosen game by over 1 billion players worldwide Competition for the game enthusiasts. Play your favourite game and win over prizes worth 15k",
     rules: [
-        "1. The competition will be supervised by the match controller and the decision of them will be final.",
-        "2. Mobile players only.",
-        "3. Solo Room Match and the map will be announced at the start if the game.",
-        "4. The competition will be conducted from the venue.",
-        "5. Hackers and emulator's will be disqualified",
-        "6. Faking or manipulating match media is forbidden.",
-        "7. Teams are responsible for their own technical issues (hardware/internet)."
+      "1. The competition will be supervised by the match controller and the decision of them will be final.",
+      "2. Mobile players only.",
+      "3. Solo Room Match and the map will be announced at the start if the game.",
+      "4. The competition will be conducted from the venue.",
+      "5. Hackers and emulator's will be disqualified",
+      "6. Faking or manipulating match media is forbidden.",
+      "7. Teams are responsible for their own technical issues (hardware/internet).",
     ],
     regFee: "50",
+    paymentLink: "https://rzp.io/l/Karma-PUBG",
     redirectLink: "/events/games/pubg",
   },
   {
@@ -24,9 +25,9 @@ const GamesLists = [
     heading: "LUDO",
     para: "KARMA'22 is here with the amazing Ludo Competition for all the Ludo Kings and Queens. Show off all your skills you acquired on quarantine with this Ludo Game and win prizes worth 30K",
     rules: [
-        "1. Competition will be done in the supervision of judges.",
-        "2. The competition will be conducted at the venue. ",
-        "3. No malpractices are allowed. ",
+      "1. Competition will be done in the supervision of judges.",
+      "2. The competition will be conducted at the venue. ",
+      "3. No malpractices are allowed. ",
     ],
     regFee: "50",
     redirectLink: "/events/games/ludo",
@@ -38,15 +39,15 @@ const GamesLists = [
     heading: "PES",
     para: "KARMA'22 Presents PES Tournament  for all the PES players. Display your skills and win prizes over 15 K.",
     rules: [
-        "1. ♦️Competition will be  conducted in the supervision of judges and the decision of them will be final. ",
-        "2. ♦️No Team Strength",
-        "3. 8 Minutes Gameplay",
-        "4. Single Match (No Home and Away)",
-        "5. Faking or manipulating match media are forbidden."
+      "1. ♦️Competition will be  conducted in the supervision of judges and the decision of them will be final. ",
+      "2. ♦️No Team Strength",
+      "3. 8 Minutes Gameplay",
+      "4. Single Match (No Home and Away)",
+      "5. Faking or manipulating match media are forbidden.",
     ],
     regFee: "50",
     redirectLink: "/events/games/pes",
-  }
+  },
 ];
 
 export default GamesLists;
