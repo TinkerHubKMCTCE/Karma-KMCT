8663072d4bbd16f6b90d3eb5fe6cdd8480bed98b
VinuBalagopalAP <vinubalagopalap@gmail.com>
2022-05-20 22:07:33 +0530

    Hackathon Razorpay 😁

--- src/pages/HackathonDetail/hackathonLists.js
+++ src/pages/HackathonDetail/hackathonLists.js
@@ -6,23 +6,24 @@ const hackathonLists = [
     heading: "FIGMA - Hackathon",
     para1:
       "A hackathon is designed to bring together a group of fresh minds to go deep into a problem, research the technology or challenge, and develop potential solutions. Mentorship is important, but teamwork and brainstorming efforts are also important.",
-      para2: "The main goal is to provide participants a hands-on experience with design in Figma by having them solve an issue and see how they can choose the best answer out of many.",
-      para3: "*PROBLEM STATEMENT: Discord UI(Design)",
-      para4: "MENTOR: RASHAD(design)",
-      para5: "Requirement: Basic knowledge in Figma.",
-      certiPara:
+    para2:
+      "The main goal is to provide participants a hands-on experience with design in Figma by having them solve an issue and see how they can choose the best answer out of many.",
+    para3: "*PROBLEM STATEMENT: Discord UI(Design)",
+    para4: "MENTOR: RASHAD(design)",
+    para5: "Requirement: Basic knowledge in Figma.",
+    certiPara:
       "It will be an 8 hr hackathon commencing from 9 am to 5 pm on 23rd of May.",
     rules: [
       "1. Indivitual participation.",
       "2. Personal laptops must be brought.",
       "3. No malpractices are allowed.",
       "4. Refresh time will be provided.",
-      "5. The result will be strictly evaluated and decided by the judging panel"
+      "5. The result will be strictly evaluated and decided by the judging panel",
     ],
     regFee: "50",
     contact1: "",
     contact2: "",
-    paymentLink: "https://rzp.io",
+    paymentLink: "https://rzp.io/l/Figma-Hackathon",
     redirectLink: "/events/hackathon/design",
   },
   {
@@ -32,23 +33,24 @@ const hackathonLists = [
     heading: "WEB - Hackathon",
     para1:
       "A hackathon is designed to bring together a group of fresh minds to go deep into a problem, research the technology or challenge, and develop potential solutions. Mentorship is important, but teamwork and brainstorming efforts are also important.",
-      para2: "The main goal is to provide participants a hands-on experience with web by having them solve an issue and see how they can choose the best answer out of many.",
-      para3: "*PROBLEM STATEMENT: will reveal soon.",
-      para4: "MENTOR: FAZIL RAHMAN(web)",
-      para5: "Requirement: Basic knowledge in Figma.",
-      certiPara:
+    para2:
+      "The main goal is to provide participants a hands-on experience with web by having them solve an issue and see how they can choose the best answer out of many.",
+    para3: "*PROBLEM STATEMENT: will reveal soon.",
+    para4: "MENTOR: FAZIL RAHMAN(web)",
+    para5: "Requirement: Basic knowledge in Figma.",
+    certiPara:
       "It will be an 8 hr hackathon commencing from 9 am to 5 pm on 23rd of May.",
     rules: [
       "1. Indivitual participation.",
       "2. Personal laptops must be brought.",
       "3. No malpractices are allowed.",
       "4. Refresh time will be provided.",
-      "5. The result will be strictly evaluated and decided by the judging panel."
+      "5. The result will be strictly evaluated and decided by the judging panel.",
     ],
     regFee: "50",
     contact1: "",
     contact2: "",
-    paymentLink: "https://rzp.io",
+    paymentLink: "https://rzp.io/l/WEB-Hackathon",
     redirectLink: "/events/hackathon/web",
   },
 ];
