86fe6912c18b51b16b1de30b321b663f6e7dc835
Hibathulla <hibathullacm@gmail.com>
2022-05-23 13:20:59 +0530

    Tech Events Added💫

--- src/pages/EventPage/EventPage.js
+++ src/pages/EventPage/EventPage.js
@@ -3,18 +3,73 @@ import Background from "../../UI/Background";
 import classes from "./EventPage.module.css";
 
 const EventPage = () => {
-    return (
-        <section className={classes.eventPage}>
-            <Background className={classes.bg}>
-
-                <div className={classes.tabs}>
-                    <div className={classes.tabList}>
-                        <div className={`${classes.tabHead}`}><NavLink to="cultural" className={({ isActive }) => (isActive ? `${classes.active} ${classes.links}` : `${classes.links}`)}>Cultural</NavLink></div>
-                        <div className={`${classes.tabHead}`}><NavLink to="technical" className={({ isActive }) => (isActive ? `${classes.active} ${classes.links}` : `${classes.links}`)}>Workshops</NavLink></div>
-                        <div className={`${classes.tabHead}`}><NavLink to="games" className={({ isActive }) => (isActive ? `${classes.active} ${classes.links}` : `${classes.links}`)}>Games</NavLink></div>
-                        <div className={`${classes.tabHead}`}><NavLink to="hackathon" className={({ isActive }) => (isActive ? `${classes.active} ${classes.links}` : `${classes.links}`)}>Hackathon</NavLink></div>
-                    </div>
-                </div>
+  return (
+    <section className={classes.eventPage}>
+      <Background className={classes.bg}>
+        <div className={classes.tabs}>
+          <div className={classes.tabList}>
+            <div className={`${classes.tabHead}`}>
+              <NavLink
+                to="cultural"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${classes.active} ${classes.links}`
+                    : `${classes.links}`
+                }
+              >
+                Cultural
+              </NavLink>
+            </div>
+            <div className={`${classes.tabHead}`}>
+              <NavLink
+                to="technical"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${classes.active} ${classes.links}`
+                    : `${classes.links}`
+                }
+              >
+                Workshops
+              </NavLink>
+            </div>
+            <div className={`${classes.tabHead}`}>
+              <NavLink
+                to="games"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${classes.active} ${classes.links}`
+                    : `${classes.links}`
+                }
+              >
+                Games
+              </NavLink>
+            </div>
+            <div className={`${classes.tabHead}`}>
+              <NavLink
+                to="hackathon"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${classes.active} ${classes.links}`
+                    : `${classes.links}`
+                }
+              >
+                Hackathon
+              </NavLink>
+            </div>
+            <div className={`${classes.tabHead}`}>
+              <NavLink
+                to="talk"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${classes.active} ${classes.links}`
+                    : `${classes.links}`
+                }
+              >
+                Expert Talk
+              </NavLink>
+            </div>
+          </div>
+        </div>
         <Outlet />
       </Background>
     </section>
